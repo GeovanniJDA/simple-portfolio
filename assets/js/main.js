2bc024583d08ae423c11107a29ade22a3f88f60a
@Geovanni_JDA <109145436+GeovanniJDA@users.noreply.github.com>
2025-11-26 20:29:41 -0300

    feat: Implement dynamic profile data loading and rendering

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,41 @@
+const Profile = {
+  photo: document.getElementById("profile.photo"),
+  name: document.getElementById("profile.name"),
+  job: document.getElementById("profile.job"),
+  location: document.getElementById("profile.location"),
+  phone: document.getElementById("profile.phone"),
+  mail: document.getElementById("profile.mail"),
+
+  softSkills: document.getElementById("profile.skills.softSkills"),
+}
+
+
+
+function updateProfileInfo(profileData) {
+  Profile.photo.src = profileData.photo
+  Profile.photo.alt = profileData.name
+  
+  Profile.name.innerText = profileData.name
+
+  Profile.job.innerText = profileData.job
+  
+  Profile.location.innerText = profileData.location
+
+  Profile.phone.innerText = profileData.phone
+  Profile.phone.href = `tel: ${profileData.phone}`
+
+  Profile.mail.innerText = profileData.mail
+  Profile.mail.href  = `mailto: ${profileData.mail}`
+  
+}
+
+function updateSoftSkills(profileData) {
+  Profile.softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
 
 (async () => {
   const profileData = await fetchProfileData()
-  console.log(profileData)
+  updateProfileInfo(profileData)
+  updateSoftSkills(profileData)
 })()
